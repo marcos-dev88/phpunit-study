d8f4e1fbea3dfe7574d9a79609d7207c011d5f55
Marcos Vitor <mv.piesske@gmail.com>
2021-10-10 23:36:37 -0300

    Configuring routes

--- api/v1/user_creator.php
+++ api/v1/user_creator.php
@@ -1,1 +1,12 @@
 <?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+/** @var AppFactory $app */
+$app->get('/test', function(Request $request, Response $response) {
+    $response->getBody()->write("Hello World! from slim");
+    return $response;
+});
+
--- public/index.php
+++ public/index.php
@@ -1,3 +1,27 @@
 <?php
 
-echo "Hello World";
+session_start();
+
+require '../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
+$app->setBasePath("/api/v1");
+
+require("../api/v1/user_creator.php");
+
+try {
+    $app->run();
+}catch (Exception $e){
+    die(json_encode(
+        [
+            "status" => "failed",
+            "message" => "This action is not allowed",
+            "exception_message" => $e
+        ]
+    ));
+}
