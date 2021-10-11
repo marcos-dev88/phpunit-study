9971ab36af2eb012451e8cd4cd38447f64583ed0
Marcos Vitor <mv.piesske@gmail.com>
2021-10-10 23:37:32 -0300

    Creating config of database and seting up the .envs config

--- src/db/Connection.php
+++ src/db/Connection.php
@@ -1,1 +1,51 @@
 <?php
+
+use PDO;
+use PDOException;
+use Throwable;
+
+require __DIR__ . '/../global.php';
+
+class Connection
+{
+
+    /**
+     * @var PDO
+     */
+    private PDO $connection;
+
+    /**
+     * @param array $connection
+     */
+    public function __construct(array $connection = [])
+    {
+        $db = getenv('SQL_DRIVER');
+        $host = getenv('DB_HOST');
+        $dbName = getenv('MYSQL_DATABASE');
+        $dbUser = getenv('DB_USERNAME');
+        $dbPassword = getenv('DB_PASSWORD');
+
+        try {
+            $this->connection = new PDO(
+              $db.':host='.$host.';dbname='.$dbName,
+              $dbUser,
+              $dbPassword,
+              [
+                  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              ]
+            );
+        }catch (PDOException $e) {
+            die(json_encode(['PDO connection Error->' => $e->getMessage()]));
+        }catch (Throwable $t) {
+            die(json_encode(['Connection Error->' => $t->getMessage()]));
+        }
+    }
+
+    /**
+     * @return PDO
+     */
+    public function getConnection(): PDO
+    {
+        return $this->connection;
+    }
+}
